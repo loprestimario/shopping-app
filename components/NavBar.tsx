bbe29ae2dccfd93b6624b0de36571847c99d29e1
Mario Lo Presti <mario84ct@hotmail.it>
2021-12-11 20:13:12 +0100

    imported icon and improved layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRssSquare } from '@fortawesome/free-solid-svg-icons'
 
 type Props = {}
 
@@ -24,48 +26,83 @@ const NavBar: React.FC<Props> = ({}) => {
 
   return (
     <>
-        <ul className="navbar-container">
-          <li>
-            <Link href='/'>
-              <a className={`nav-bar ${asPath === '/' ? 'active' : ''}`}>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/about' passHref>
-              <a className={`nav-bar ${asPath === '/about' ? 'active' : ''}`}>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/portofolio'>
-              <a className={`nav-bar ${asPath === '/portofolio' ? 'active' : ''}`}>Portofolio</a>
-            </Link>
-          </li>
-          {/*<li>*/}
-          {/*  <Link href='/blog'>*/}
-          {/*    <a className={`nav-bar ${asPath === '/blog' ? 'active' : ''}`}>Blog</a>*/}
-          {/*  </Link>*/}
-          {/*</li>*/}
-          <li>
-            <Link href='/contact'>
-              <a className={`nav-bar ${asPath === '/contact' ? 'active' : ''}`}>Contact</a>
-            </Link>
-          </li>
-        </ul>
+      <nav className='navbar'>
+        <div className='navbar-container'>
+          <Link href='/'>
+            <a className={`navbar-logo ${asPath === '/' ? 'active' : ''}`}>
+              <span className="home-page">Web App</span>
+              <span className="icon-container">
+                  <FontAwesomeIcon icon={faRssSquare} size="1x"/>
+                </span>
+            </a>
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}/>
+          </div>
+          <ul className="nav-menu">
+            <li>
+              <Link href='/about' passHref>
+                <a className={`nav-bar ${asPath === '/about' ? 'active' : ''}`}>About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/portofolio'>
+                <a className={`nav-bar ${asPath === '/portofolio' ? 'active' : ''}`}>Portofolio</a>
+              </Link>
+            </li>
+            {/*<li>*/}
+            {/*  <Link href='/blog'>*/}
+            {/*    <a className={`nav-bar ${asPath === '/blog' ? 'active' : ''}`}>Blog</a>*/}
+            {/*  </Link>*/}
+            {/*</li>*/}
+            <li>
+              <Link href='/contact'>
+                <a className={`nav-bar ${asPath === '/contact' ? 'active' : ''}`}>Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
       <style jsx>{`
+        .navbar {
+          display: flex;
+          width: 100%;
+          height: 80px;
+          background: linear-gradient(90deg, darkgrey 0%, lightgray 100%);
+          margin: 0;
+          font-size: 1.1rem;
+          position: sticky;
+          top: 0;
+          z-index: 999;
+        }
         .navbar-container {
+          width: 100%;
+          margin: 0 30px;
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          align-items: center;
+        }
+        .navbar-logo {
+          display: flex;
+          align-items: center;
+        }
+        .home-page {
+          width: 90px;
+        }
+        .icon-container {
+          width: 35px;
+        }
+        .nav-menu {
           display: flex;
           flex-direction: row;
           justify-content: space-between;
           align-items: center;
           width: 100%;
-          height: 80px;
-          background-color: black;
-          color: white;
-          margin: 0;
         }
         li {
-        list-style: none;
-        padding: 0rem 2rem;
+          list-style: none;
+          padding: 0rem 2rem;
         }
         .active {
           color: blue;
